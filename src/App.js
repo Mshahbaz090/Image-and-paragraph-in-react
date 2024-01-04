db1d34ca077d7804a179e97beaa2b8bd7dac2343
Shabaz123 <shahbaziub1377@gmail.com>
2024-01-04 15:32:38 +0500

    first comit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react"
+import "./App.css"
+import image from './images/rwp.jpg'
+
+function App() {
+  return (
+    <div>
+      <h1 className="abc"><b><i>RDA Takes Strong Action Against 16 Illegal Housing Schemes in Rawalpindi</i></b>
+        <img src={image} alt=""></img>
+      </h1>
+
+      <textField />
+
+      <p>The operation, which took place in Mouzas Adhwal, Cighu, Chak Cighu, Bhal, Titral, Hoon, Gojra Chak, Ralla, Ralla Gujra, Banda, Wariyam Malokra, Gurha Bertha, Kuri Khuda Bakhsh, and Bagga Sheikhan, involved the demolition of main entrances, guard rooms, boundary walls, dividers, and road infrastructure belonging to these unauthorized schemes.
+
+        ALSO READ
+        Cash Van Driver Steals Over Rs. 6 Crore in Karachi
+        The RDA spokesperson confirmed that the targeted illegal housing schemes included Hoon Farm, Theme Park, Cighu Farm House, Green Hawks/Flaura Farms, Freedom Smart Home, Islamabad Forms Phase-I/II, Shahzain Town, Atlantic City, Attar Farms, Project of BSM Developers, Green View, Lake View City/Lake Garden Green Farm House, Green Stone, and Gulberg Garden in Rawat Rawalpindi.
+
+        Ad powered by advergic.com
+
+      </p>
+    </div>
+  )
+}
+
+export default App
